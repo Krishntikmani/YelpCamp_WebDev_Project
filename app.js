cf175140641b07da4fd98eb57b4e6602af7c4526
root <kanhatikmani@gmail.com>
2020-06-01 10:57:23 +0000

    updated port

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.use(commentRoutes);
 
 
 //Server running command
-app.listen(3000,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
 	console.log("YelpCamp on its way!!!");
 });
